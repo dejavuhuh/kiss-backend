76aaee5a97575f01b88dc81ba62b19b49c750e1e
wuyue7570 <wuyue@musetech.group>
2025-04-01 19:17:40 +0800

    feat(authentication): 实现会话历史分页查询功能

--- src/main/kotlin/kiss/authentication/SessionHistoryService.kt
+++ src/main/kotlin/kiss/authentication/SessionHistoryService.kt
@@ -1,6 +1,9 @@
 package kiss.authentication
 
+import kiss.authentication.dto.SessionHistorySpecification
+import org.babyfish.jimmer.Page
 import org.babyfish.jimmer.sql.kt.KSqlClient
+import org.babyfish.jimmer.sql.kt.fetcher.newFetcher
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,7 +19,20 @@ class SessionHistoryService(val sql: KSqlClient) {
     fun list(
         @RequestParam pageIndex: Int,
         @RequestParam pageSize: Int,
-    ) {
+        specification: SessionHistorySpecification
+    ): Page<SessionHistory> {
+        return sql.createQuery(SessionHistory::class) {
+            where(specification)
+            select(table.fetch(LIST_ITEM))
+        }.fetchPage(pageIndex, pageSize)
+    }
 
+    companion object {
+        val LIST_ITEM = newFetcher(SessionHistory::class).by {
+            allScalarFields()
+            user {
+                username()
+            }
+        }
     }
 }
