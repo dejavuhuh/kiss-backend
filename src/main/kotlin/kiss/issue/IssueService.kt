a63439d12d895e36b9f571b61d22e04e1cca8dc5
wuyue <wuyue19970930@gmail.com>
2025-04-07 03:02:14 +0800

    feat(issue): 新增Issue查询功能及规范
    在Issue.dto中添加查询规范，并在IssueService中实现分页查询和详情查询接口

--- src/main/kotlin/kiss/issue/IssueService.kt
+++ src/main/kotlin/kiss/issue/IssueService.kt
@@ -1,13 +1,15 @@
 package kiss.issue
 
 import kiss.issue.dto.IssueInput
+import kiss.issue.dto.IssueSpecification
 import kiss.jimmer.insertOnly
+import org.babyfish.jimmer.Page
+import org.babyfish.jimmer.client.FetchBy
 import org.babyfish.jimmer.sql.kt.KSqlClient
+import org.babyfish.jimmer.sql.kt.ast.expression.desc
+import org.babyfish.jimmer.sql.kt.fetcher.newFetcher
 import org.springframework.transaction.annotation.Transactional
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @Transactional
 @RestController
@@ -17,5 +19,37 @@ class IssueService(val sql: KSqlClient) {
     @PostMapping
     fun report(@RequestBody input: IssueInput) {
         sql.insertOnly(input)
+    }
+
+    @GetMapping("/{id}")
+    fun get(@PathVariable id: Int): @FetchBy("GET") Issue {
+        return sql.findOneById(GET, id)
+    }
+
+    @GetMapping
+    fun list(
+        @RequestParam pageIndex: Int,
+        @RequestParam pageSize: Int,
+        specification: IssueSpecification,
+    ): Page<@FetchBy("LIST_ITEM") Issue> = sql.createQuery(Issue::class) {
+        where(specification)
+        orderBy(table.id.desc())
+        select(table.fetch(LIST_ITEM))
+    }.fetchPage(pageIndex, pageSize)
+
+    companion object {
+        val LIST_ITEM = newFetcher(Issue::class).by {
+            title()
+            createdTime()
+            creator {
+                username()
+            }
+        }
+        val GET = newFetcher(Issue::class).by {
+            allScalarFields()
+            creator {
+                username()
+            }
+        }
     }
 }
